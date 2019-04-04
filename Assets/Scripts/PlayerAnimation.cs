32b34914caa32cbafab29ce20fdebdfebdb88f9b
Zion Perez <zionperez@gmail.com>
2019-04-05 00:10:47 +0900

    Complete player setup section.

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private Animator swordAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        swordAnimator = transform.GetChild(1).GetComponent<Animator>();
+    }
+
+    public void Move(float move)
+    {
+        animator.SetFloat("Move", Mathf.Abs(move));
+    }
+
+    public void Jump(bool jump)
+    {
+        animator.SetBool("Jump", jump);
+    }
+
+    public void Attack()
+    {
+        animator.SetTrigger("Attack");
+        swordAnimator.SetTrigger("SwordAnimation");
+    }
+}
